b8470edfdd112ac4be7d735e31ac66bfbc25c985
Clara Raef Nasry <clara180550@feng.bu.edu.eg>
2022-05-16 16:51:12 +0200

    Update gpio.h

--- embedded project/gpio.h
+++ embedded project/gpio.h
@@ -6,12 +6,22 @@
 #include <stdlib.h>
 #include <time.h>
 #include "inter.h"
-//#include "Nokia5110.h"	
+#include "Nokia5110.h"	
 
 /* section: functions declarations */
+int TimerCount, Semaphore;
+int delay;
+int m;
+volatile int addData;
+
+void EnableInterrupts(void);  // Enable interrupts
 void EdgeCounter_Init(void);
 void PortF_Init(void);
-
-
+void Timer2_Init(unsigned long period);
+void Delay100ms(unsigned long count);
+void ADC_Init(void);
+void ADC0Seq3_Handler(void){
+ 
+} 
 #endif
 
