e810f90e3e213677a0423ae5d0a71982ffe27f96
Clara Raef Nasry <clara180550@feng.bu.edu.eg>
2022-05-16 16:50:32 +0200

    Update gpio.c

--- embedded project/gpio.c
+++ embedded project/gpio.c
@@ -1,5 +1,7 @@
 #include "gpio.h"
 
+
+
 void EdgeCounter_Init(void){                          
   SYSCTL_RCGC2_R |= 0x00000020; // (a) activate clock for port F
   GPIO_PORTF_DIR_R &= ~0x10;    // (c) make PF4 in (built-in button)
@@ -44,4 +46,70 @@ void PortF_Init(void){ volatile unsigned long delay;
   GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital pins PF4-PF0         
 }
 
+// You can use this timer only if you learn how it works
+void Timer2_Init(unsigned long period){ 
+  unsigned long volatile delay;
+  SYSCTL_RCGCTIMER_R |= 0x04;   // 0) activate timer2
+  delay = SYSCTL_RCGCTIMER_R;
+  TimerCount = 0;
+  Semaphore = 0;
+  TIMER2_CTL_R = 0x00000000;    // 1) disable timer2A during setup
+  TIMER2_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
+  TIMER2_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
+  TIMER2_TAILR_R = period-1;    // 4) reload value
+  TIMER2_TAPR_R = 0;            // 5) bus clock resolution
+  TIMER2_ICR_R = 0x00000001;    // 6) clear timer2A timeout flag
+  TIMER2_IMR_R = 0x00000001;    // 7) arm timeout interrupt
+  NVIC_PRI5_R = (NVIC_PRI5_R&0x00FFFFFF)|0x80000000; // 8) priority 4
+// interrupts enabled in the main program after all devices initialized
+// vector number 39, interrupt number 23
+  NVIC_EN0_R = 1<<23;           // 9) enable IRQ 23 in NVIC
+  TIMER2_CTL_R = 0x00000001;    // 10) enable timer2A
+}
+void Timer2A_Handler(void){ 
+  TIMER2_ICR_R = 0x00000001;   // acknowledge timer2A timeout
+  TimerCount++;
+  Semaphore = 1; // trigger
+}
+void Delay100ms(unsigned long count){unsigned long volatile time;
+  while(count>0){
+    time = 227240;  // 0.1sec at 80 MHz
+    while(time){
+	  	time--;
+    }
+    count--;
+  }
+}
 
+void ADC_Init(void){
+	
+	SYSCTL_RCGCADC_R |= (1<<0);
+	SYSCTL_RCGCGPIO_R|= (1<<4)
+												|(1<<5);
+	delay=SYSCTL_RCGCGPIO_R;
+	
+	GPIO_PORTE_AFSEL_R |=(1<<2);
+
+	GPIO_PORTE_DEN_R &= ~(1<<2);
+	
+	GPIO_PORTE_AMSEL_R |= (1<<2);
+	
+	ADC0_ACTSS_R &= ~(1<<3);
+	
+	ADC0_EMUX_R = (0XF<<12);
+	
+	
+	ADC0_SSMUX3_R = 1;
+	
+	ADC0_SSCTL3_R |=(1<<1)
+								|(1<<2);
+		
+	ADC0_IM_R |=(1<<2);
+	
+	ADC0_RIS_R |=(1<<3);
+	
+ 	ADC0_ACTSS_R |= (1<<3);	
+
+  NVIC_EN2_R |= (1<<3);
+	
+}
